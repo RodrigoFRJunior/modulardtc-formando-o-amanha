bb50041477960c4a8498a963d798f1676d164e42
Rodrigo Felipe Rodrigues Junior <rodrigofrj0@gmail.com>
2025-03-03 16:34:36 -0300

    1.0

---
+++ js/buttonHomePage.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".botão_registrar").addEventListener("click", registrarLogin);
+    document.querySelector(".botão_continuar").addEventListener("click", validarLogin);
+});
